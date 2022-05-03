0d9bf34ddabcd52e3636d2b549d74429b10b47d4
Kei Miyamoto <k.miyamoto95@gmail.com>
2022-05-03 14:38:01 +0900

    コンポーネントの再利用

---
+++ component/component.js
@@ -0,0 +1,48 @@
+
+
+Vue.component('fruits-list-title',{
+  template:'<h1>フルーツ一覧</h1>',
+})
+Vue.component('fruits-list-description',{
+  template:'<p>季節の代表的なフルーツの一覧です</p>',
+})
+Vue.component('fruits-list-table',{
+  template:`
+  <table>
+    <thead>
+      <tr>
+        <th>季節</th>
+        <th>フルーツ</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>春</t>
+        <td>いちご</td>
+      </tr>
+      <tr>
+        <td>夏</td>
+        <td>スイカ</td>
+      </tr>
+      <tr>
+        <td>秋</td>
+        <td>ぶどう</td>
+      </tr>
+      <tr>
+        <td>冬</td>
+        <td>みかん</td>
+      </tr>
+    </tbody>
+  </table>
+  `,
+})
+
+new Vue({
+  el:'#fruits-list'
+})
+// var FruitsListTitle = Vue.extend({
+//   template:'<h1>フルーツ一覧</h1>',
+// })
+// new FruitsListTitle().$mount('#fruits-list')
+
+
