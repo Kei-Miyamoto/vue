af09e5f154eabcc876ef794ce6692d0dd29f92c4
Kei Miyamoto <k.miyamoto95@gmail.com>
2022-05-13 23:56:25 +0900

    no message

--- route/route.js
+++ route/route.js
@@ -1,3 +1,45 @@
+var getUsers = function(callback) {
+  setTimeout(function(){
+    callback(null,[{
+        id:1,
+        name:'Joddy America'
+      },{
+        id:2,
+        name:'Kim Korea'
+      }
+    ])
+  }, 1000)
+}
+
+var UserList = {
+  template:'#user-list',
+  data: function() {
+    return{
+      loading: false,
+      users: function(){return[]},
+      error:null
+    }
+  },
+  created: function() {
+    this.fetchData()
+  },
+  watch: {
+    '$route':'fetchData'
+  },
+  methods:{
+    fetchData: function(){
+      this.loading = true;
+      getUsers((function(err,users) {
+      this.loading = false;
+      if(err) {
+      this.error = err.toString()
+      }else {
+      this.users = users
+      }
+      }).bind(this))
+    }
+  }
+}
 
 var router = new VueRouter({
   routes:[{
@@ -8,13 +50,19 @@ var router = new VueRouter({
   },
   {
     path:'/users',
+    component: UserList
+  },
+  {
+    path:'/user/:userId',
+    name:'user',
     component: {
-      template:'<div>Thi is User page</div>'
+      template:'<div>User ID is {{ $route.params.userId }}</div>'
     }
   }
-
   ]
 })
+
+
 
 var app = new Vue({
   router: router
