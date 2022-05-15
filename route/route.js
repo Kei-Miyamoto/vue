7e995a326211db2f2e6b3471bb40d67469c83682
Kei Miyamoto <k.miyamoto95@gmail.com>
2022-05-15 12:04:46 +0900

    新規登録

--- route/route.js
+++ route/route.js
@@ -1,3 +1,16 @@
+var userData = [
+  {
+    id:1,
+    name:'Joddy America',
+    description:'I work as engineer in America'
+  },
+  {
+    id:2,
+    name:'Kim Korea',
+    description:'I am eingneer, My hobby is football'
+  }
+]
+
 var getUsers = function(callback) {
   setTimeout(function(){
     callback(null,[{
@@ -9,6 +22,23 @@ var getUsers = function(callback) {
       }
     ])
   }, 1000)
+}
+
+var getUser = function(userId,callback) {
+  setTimeout(function(){
+    var filteredUsers = userData.filter(function (user) {
+      return user.id === parselnt(userId,10)
+    })
+    callback(null, filteredUsers && filteredUsers[0])
+  }, 1000)
+}
+
+var postUser= function(params,callback) {
+  setTimeout(function() {
+    params.id = userData.length + 1
+    userData.pish(params)
+    callback(null,params)
+  },1000)
 }
 
 var UserList = {
@@ -28,14 +58,89 @@ var UserList = {
   },
   methods:{
     fetchData: function(){
-      this.loading = true;
+      this.loading = true
       getUsers((function(err,users) {
-      this.loading = false;
+      this.loading = false
       if(err) {
       this.error = err.toString()
       }else {
       this.users = users
       }
+      }).bind(this))
+    }
+  }
+}
+
+var UserDetail = {
+  template: '#user-detail',
+  data: function() {
+    return {
+      loading: false,
+      user: null,
+      error: null,
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  watch:{
+    '$route':'fetchData'
+  },
+  methods: {
+    fetchData: function() {
+      this.loading = true
+      getUser(this.$route.params.userId,(function(err,user) {
+        this.loading = false
+        if(err) {
+        this.error = err.toString()
+        }else {
+        this.user = user
+        }
+      }).bind(this))
+    }
+  }
+}
+
+
+
+var UserCreate = {
+  template:'#user-create',
+  data: function() {
+    return{
+      sending: false,
+      user: this.defaultUser(),
+      error: null
+    }
+  },
+  created: function() {
+    
+  },
+  methods: {
+    defaultUser: function() {
+      return{
+        name: "",
+        description: ""
+      }
+    },
+    createUser: function(){
+      if(this.user.name.trim() === '') {
+        this.error = 'Name is required'
+        return
+      }
+      if(this.user.description.trim() === '') {
+        this.error = 'Description is required'
+        return
+      }
+      postUser(this.user,(function(err,user) {
+        this.sending = false
+        if(err) {
+          this.error = err.toString()
+        }else {
+          this.error = null
+          this.user = this.defaultUser()
+          alert('Created new user')
+          this.$route.push('/users')
+        }
       }).bind(this))
     }
   }
@@ -53,11 +158,12 @@ var router = new VueRouter({
     component: UserList
   },
   {
-    path:'/user/:userId',
-    name:'user',
-    component: {
-      template:'<div>User ID is {{ $route.params.userId }}</div>'
-    }
+    path:'/users/new',
+    component:UserCreate
+  },
+  {
+    path:'/users/:userId',
+    component: UserDetail
   }
   ]
 })
